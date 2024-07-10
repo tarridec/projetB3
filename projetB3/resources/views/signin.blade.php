8989ca912b0d75025dedcbb0e45e81215e8376c5
ismael-dmn <ismael.dahmane@icloud.com>
2024-07-10 11:43:21 +0200

    Header complété

---
+++ projetB3/resources/views/signin.blade.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="fr">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <link rel="stylesheet" href="{{ asset('css/app.css') }}">
+        <title>Sign In Page</title>
+    </head>
+
+    <body>
+        <div>@include('partials.navbar')<div>
+
+        <h1>Sign In Page</h1>
+
+        <!-- 
+        *
+        * Body
+        *
+        -->
+
+        <div>@include('partials.footer')<div>
+    </body>
+</html>
--- projetB3/webpack.mix.js
+++ projetB3/webpack.mix.js
@@ -1,5 +1,3 @@
-const mix = require('laravel-mix');
-
 /*
  |--------------------------------------------------------------------------
  | Mix Asset Management
@@ -10,8 +8,11 @@ const mix = require('laravel-mix');
  | file for the application as well as bundling up all the JS files.
  |
  */
-
-mix.js('resources/js/app.js', 'public/js')
-    .postCss('resources/css/app.css', 'public/css', [
-        //
-    ]);
+ const mix = require('laravel-mix');
+ const tailwindcss = require('tailwindcss');
+ 
+ mix.js('resources/js/app.js', 'public/js')
+     .postCss('resources/css/app.css', 'public/css', [
+         tailwindcss('./tailwind.config.js'),
+     ]);
+ 
